630a416f865c0cf7f7dfaf5691d17f8e62f2a865
Vitaly Ruhl <66111567+vitalyruhl@users.noreply.github.com>
2021-10-28 19:15:41 +0200

    oops... wrong publisher...

--- publish.sh
+++ publish.sh
@@ -3,5 +3,9 @@ npm install -g vsce
 vsce package
 vsce publish
 
+
+#https://marketplace.visualstudio.com/manage/publishers/Vitaly-ruhl
+
+
 #https://code.visualstudio.com/api/working-with-extensions/publishing-extension
-#"publisher": "VSCEViRu",
+#"publisher": "Vitaly-ruhl",
